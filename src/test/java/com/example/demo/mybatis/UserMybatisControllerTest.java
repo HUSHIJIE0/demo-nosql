2f45986989a7a8417e94dd2fa8cd0643c6d0c6ba
HUSHIJIE0 <244198238@qq.com>
2018-11-28 10:14:14 +0800

    Junit

---
+++ src/test/java/com/example/demo/mybatis/UserMybatisControllerTest.java
@@ -0,0 +1,42 @@
+package com.example.demo.mybatis;
+
+import com.example.demo.DemoApplication;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.Assert.*;
+
+/**
+ * @Author: Michael
+ * @Date: Created in 10:07 2018/11/28
+ * @Desciption:
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = DemoApplication.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@WebAppConfiguration
+public class UserMybatisControllerTest {
+    @Autowired
+    private UserMybatisController userMybatisController;
+
+
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("This is Before!");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("This is After!");
+    }
+
+    @Test
+    public void toIndex() {
+        userMybatisController.toIndex(1);
+    }
+}
